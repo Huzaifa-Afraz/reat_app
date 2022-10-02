4160a8b2abd6ca53222dc4b181627db21d3be70e
Huzaifa-Afraz <95672012+Huzaifa-Afraz@users.noreply.github.com>
2022-10-02 11:06:38 +0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import emojiPedia from "../emojipedia";
+import Entry from "./entry";
+// {
+//   id: 1,
+//   emoji: "💪",
+//   name: "Tense Biceps",
+//   meaning:
+//     "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+// }
+function createemoji(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      dis={emojiTerm.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojiPedia.map(createemoji)}</dl>
+    </div>
+  );
+}
+
+export default App;
